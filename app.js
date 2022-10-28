57bcb94bf16431aab808402b7b5fd13c218f0c4f
Harish <106601387+harish4462@users.noreply.github.com>
2022-10-28 22:22:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
